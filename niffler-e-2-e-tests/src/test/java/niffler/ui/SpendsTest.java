2270ee990cecc22ff53f7b19f91760ea4b3a4a94
MichaelMironov <nemiron94@gmail.com>
2023-01-25 17:14:19 +0300

    add: homework1

---
+++ niffler-e-2-e-tests/src/test/java/niffler/ui/SpendsTest.java
@@ -0,0 +1,33 @@
+package niffler.ui;
+
+import niffler.BaseTest;
+import niffler.database.dao.SpendsDao;
+import niffler.database.entity.Spends;
+import niffler.jupiter.di.Inject;
+import niffler.jupiter.di.Injector;
+import niffler.ui.steps.CheckSteps;
+import niffler.ui.steps.LoginSteps;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Injector
+class SpendsTest extends BaseTest {
+
+    @Inject(amount = 5000.0, currency = "RUB", category = "Бары", description = "Тест добавления затрат")
+    Spends spends;
+
+    @Test
+    void test() {
+
+        CheckSteps.findAllSpends();
+        LoginSteps.login();
+        CheckSteps.checkSpends(spends.getSpendDate(), "5000", "RUB", "Бары", "Тест добавления затрат");
+    }
+
+    @AfterEach
+    void after() {
+        Assertions.assertTrue(SpendsDao.getInstance().clear());
+        CheckSteps.findAllSpends();
+    }
+}
