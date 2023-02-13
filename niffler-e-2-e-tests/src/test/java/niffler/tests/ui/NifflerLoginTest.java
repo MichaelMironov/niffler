c2571f0c362f0de29bb9c65e48a60450f5f44b64
MichaelMironov <nemiron94@gmail.com>
2023-02-13 08:54:22 +0300

    refactor: added user's supplier extension

--- niffler-e-2-e-tests/src/test/java/niffler/tests/ui/NifflerLoginTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/tests/ui/NifflerLoginTest.java
@@ -3,26 +3,25 @@ package niffler.tests.ui;
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.AllureId;
 import lombok.extern.slf4j.Slf4j;
-import niffler.jupiter.context.User;
-import niffler.jupiter.context.UsersExtension;
-import niffler.model.UserModel;
+import niffler.jupiter.di.user.User;
+import niffler.jupiter.di.user.UsersSupplier;
+import niffler.data.model.UserModel;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
-import static niffler.jupiter.context.User.UserType.ADMIN;
-import static niffler.jupiter.context.User.UserType.COMMON;
+import static niffler.jupiter.di.user.User.UserType.ADMIN;
+import static niffler.jupiter.di.user.User.UserType.COMMON;
 
 @Slf4j
-@ExtendWith(UsersExtension.class)
+@ExtendWith(UsersSupplier.class)
 class NifflerLoginTest extends BaseTest {
 
     @AllureId("1")
     @Test
     void test_1(@User(COMMON) UserModel userModel) {
-        log.info("Test_1. " + userModel.toString());
         System.out.println("Test_1. " + userModel);
         step("Check login", () -> {
             open(PROPS.authUrl());
@@ -38,10 +37,7 @@ class NifflerLoginTest extends BaseTest {
     @AllureId("2")
     @Test
     void test_2(@User(COMMON) UserModel first, @User(ADMIN) UserModel second) {
-        log.info("Test_2. " + first.toString());
-        log.info("Test_2. " + second.toString());
-        System.out.println("Test_2. " + first);
-        System.out.println("Test_2. " + second);
+        System.out.println("Test_2. Users: " + first + "\t" + second);
         step("Check login", () -> {
             open(PROPS.authUrl());
             $("a[href*='redirect']").click();
@@ -55,8 +51,7 @@ class NifflerLoginTest extends BaseTest {
 
     @AllureId("3")
     @Test
-    void test_3(@User UserModel userModel) {
-        log.info("Test_3. " + userModel.toString());
+    void test_3(@User(COMMON) UserModel userModel) {
         System.out.println("Test_3. " + userModel);
         step("Check login", () -> {
             open(PROPS.authUrl());
@@ -71,8 +66,7 @@ class NifflerLoginTest extends BaseTest {
 
     @AllureId("4")
     @Test
-    void test_4(@User UserModel userModel) {
-        log.info("Test_4. " + userModel.toString());
+    void test_4(@User(COMMON) UserModel userModel) {
         System.out.println("Test_4. " + userModel);
         step("Check login", () -> {
             open(PROPS.authUrl());
@@ -87,9 +81,23 @@ class NifflerLoginTest extends BaseTest {
 
     @AllureId("5")
     @Test
-    void test_5(@User UserModel userModel) {
-        log.info("Test_5. " + userModel.toString());
+    void test_5(@User(COMMON) UserModel userModel) {
         System.out.println("Test_5. " + userModel);
+        step("Check login", () -> {
+            open(PROPS.authUrl());
+            $("a[href*='redirect']").click();
+            $("input[name='username']").setValue("test");
+            $("input[name='password']").setValue("12345");
+            $("button[type='submit']").click();
+            $(".header__title").shouldBe(Condition.visible)
+                    .shouldHave(Condition.text("Niffler. The coin keeper."));
+        });
+    }
+
+    @AllureId("6")
+    @Test
+    void test_6(@User(COMMON) UserModel userModel) {
+        System.out.println("Test_6. " + userModel);
         step("Check login", () -> {
             open(PROPS.authUrl());
             $("a[href*='redirect']").click();
