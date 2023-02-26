37bdb1c6e2fe2371d1634d61eddc8f4916341452
MichaelMironov <nemiron94@gmail.com>
2023-02-26 22:17:27 +0300

    refactor: renamed api service objects

--- niffler-e-2-e-mironov-tests/src/test/java/niffler/tests/api/ApiTests.java
+++ niffler-e-2-e-mironov-tests/src/test/java/niffler/tests/api/ApiTests.java
@@ -6,9 +6,7 @@ import niffler.data.json.SpendJson;
 import niffler.data.json.UserJson;
 import niffler.jupiter.converters.spend.Spend;
 import niffler.jupiter.converters.user.User;
-import niffler.jupiter.di.client.Client;
 import niffler.jupiter.di.client.ClientSupplier;
-import niffler.tests.ui.steps.LoginSteps;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -19,17 +17,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(ClientSupplier.class)
-public class SpendApiTest {
+public class ApiTests {
 
-    private @Client
-    SpendClient spendClient;
-    private @Client
-    UserClient userClient;
+    SpendClient spendClient = new SpendClient();
+    UserClient userClient = new UserClient();
 
     @ValueSource(strings = {"data/spend.json", "data/spend1.json"})
     @ParameterizedTest
-    void addSpend(@Spend SpendJson spend) throws IOException {
-        final SpendJson created = spendClient.createSpendJson(spend);
+    void addSpend(@Spend SpendJson spend) throws Exception {
+        final SpendJson created = spendClient.createSpend(spend);
         assertNotNull(created.getId());
     }
 
