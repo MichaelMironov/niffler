8cc5acf380bec42c7d534dde04152fce1810af23
MichaelMironov <nemiron94@gmail.com>
2023-03-08 13:15:07 +0300

    added userdata repository

--- niffler-e-2-e-mironov-tests/src/test/java/niffler/database/dao/UserdataRepository.java
+++ niffler-e-2-e-mironov-tests/src/test/java/niffler/database/dao/UserdataRepository.java
@@ -1,6 +1,7 @@
 package niffler.database.dao;
 
 import niffler.database.DataBase;
+import niffler.database.entity.userdata.ProfileEntity;
 import niffler.database.jpa.EmfContext;
 import niffler.database.jpa.JpaService;
 
@@ -8,5 +9,21 @@ public class UserdataRepository extends JpaService {
 
     public UserdataRepository() {
         super(EmfContext.INSTANCE.getEmf(DataBase.USERDATA).createEntityManager());
+    }
+
+    public void addProfiles(ProfileEntity... profileEntities) {
+        for (ProfileEntity profileEntity : profileEntities) {
+            persist(profileEntity);
+        }
+    }
+
+    public void updateProfile(ProfileEntity profileEntity) {
+        merge(profileEntity);
+    }
+
+    public void removeAll(ProfileEntity... profileEntities) {
+        for (ProfileEntity profileEntity : profileEntities) {
+            remove(profileEntity);
+        }
     }
 }
